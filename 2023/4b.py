a94cb0752e65a4f8d0c18fa29d86c0abd873015f
Vilhelm Malmberg Eskilsson <vima23@student.bth.se>
2023-12-04 14:27:28 +0100

    Made 4b a million times faster

--- 2023/4b.py
+++ 2023/4b.py
@@ -1,24 +1,35 @@
 """ Solution for 4b """
-
 from aocd import get_data
 
-def main():
-    """ Main function for day 4b of 2023 """
-    data = get_data(day=4, year=2023).split("\n")
-    extra_cards = list(range(1, len(data)+1))
+def card_dict_maker(data):
+    """Turns the data into a dict for faster processing"""
+    card_dict = {}
+
     for card, line in enumerate(data, 1):
         winning_numbers, card_numbers = line.split("|")
-        winning_numbers = winning_numbers.split(":")[1].split()
-        card_numbers = card_numbers.split()
-        for i in range(extra_cards.count(card)):
-            wins = 0
-            for number in winning_numbers:
-                wins += card_numbers.count(number)
-            for i in range(1, wins+1):
-                extra_cards.append(card+i)
-    print(len(extra_cards))
-    total_cards = [card for card in extra_cards if card <= len(data)+1]
-    print(len(total_cards))
+        winning_numbers = set(winning_numbers.split(":")[1].split())
+        card_numbers = set(card_numbers.split())
+        wins = len(winning_numbers.intersection(card_numbers))
+        copies = 1
+        card_dict.update({card: [wins, copies]})
+
+    return card_dict
+
+def win_checker(card_dict):
+    """Simulates the ticket chain reaction"""
+    total_copies = 0
+    for i in card_dict:
+        total_copies += card_dict.get(i)[1]
+        for j in range(1, card_dict.get(i)[0]+1):
+            card_dict[i+j][1] += 1*card_dict.get(i)[1]
+    return total_copies
+
+def main():
+    """Main function for 4b"""
+    data = get_data(day=4, year=2023).split("\n")
+    #data = test_data.split("\n")
+    card_dict = card_dict_maker(data)
+    print(win_checker(card_dict))
 
 if __name__ == "__main__":
     main()
